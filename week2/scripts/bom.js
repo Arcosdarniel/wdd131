407bd6afff82e3c9d487fa2c7b525d481e738dda
Arcosdarniel <danielorlando772@gmail.com>
2026-01-12 00:06:50 -0300

    jsbok

---
+++ week2/scripts/bom.js
@@ -0,0 +1,26 @@
+
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', function () {
+
+    if (input.value.trim() === '') {
+        input.focus();
+    }
+    else {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '❌';
+        li.textContent = input.value;
+        li.append(deleteButton);
+        list.append(li);
+        input.value = '';
+        input.focus();
+
+        deleteButton.addEventListener('click', function () {
+            li.remove();
+            input.focus();
+        });
+    }
+});
